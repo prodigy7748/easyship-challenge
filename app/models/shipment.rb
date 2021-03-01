fe3e41a048aa72a0a9a51d80aa0c093dc9c3b395
Phil Lee <prodigy7748g@gmail.com>
2021-03-01 23:03:18 +0800

    using delegate prefix to make it more readable

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,7 +2,7 @@ class Shipment < ApplicationRecord
   belongs_to :company
   has_many :shipment_items
 
-  delegate :name, to: :company
+  delegate :name, to: :company, prefix: true
 
   def group_shipment_items
     shipment_items.group(:description).count.sort_by{ |key, value| value }.reverse.map{ |item, quantity| {description: item, count: quantity} }
