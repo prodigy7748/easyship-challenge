443fc4bf7189b0a39df28428eaf1d4af336cc73f
Phil Lee <prodigy7748g@gmail.com>
2021-03-01 22:31:00 +0800

    add find_company and includes shipment_items for shipment index

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,15 +1,21 @@
 class ShipmentsController < ApplicationController
   require 'aftership'
+  before_action :find_company, only: [:index, :show]
 
   def index
-    @shipments = Shipment.all
+    @shipments = @company.shipments.includes(:shipment_items)
   end
 
   def show
-    @company = Company.find_by(id: params[:company_id])
     @shipment = @company.shipments.find_by(id: params[:id])
 
     AfterShip.api_key = ENV['AFTERSHIP_API_KEY']
     @tracking = AfterShip::V4::Tracking.get(@shipment.slug, @shipment.tracking_number)
+  end
+
+  private
+
+  def find_company
+    @company = Company.find_by(id: params[:company_id])
   end
 end
