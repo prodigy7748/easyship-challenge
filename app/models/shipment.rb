e07e1130aecaea73cbda8fdd84e3b509a912ef21
Phil Lee <prodigy7748g@gmail.com>
2021-03-02 09:45:35 +0800

    set column validates for all models

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,6 +2,11 @@ class Shipment < ApplicationRecord
   belongs_to :company
   has_many :shipment_items
 
+  validates :destination_country, presence: true
+  validates :origin_country,      presence: true
+  validates :tracking_number,     presence: true
+  validates :slug,                presence: true
+
   delegate :name, to: :company, prefix: true
 
   def group_shipment_items
