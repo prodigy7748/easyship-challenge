5089c34cb84ca307ab1646b94efa6317af7b5fb2
Phil Lee <prodigy7748g@gmail.com>
2021-03-02 11:48:45 +0800

    add routes and before_action test

--- spec/controllers/shipments_controller.rb
+++ spec/controllers/shipments_controller.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ShipmentsController do
-  it 'can get right tracking info' do
-    shipment = FactoryBot.create(:shipment, 'slug': 'usps', 'tracking_number': '92748902711539543475379057')
-    get :show, params: { company_id: shipment.company.id, id: shipment.id }
-    tracking = AfterShip::V4::Tracking.get(shipment.slug, shipment.tracking_number)
 
-    expect(tracking["data"]["tracking"]["tag"]).to  eq("Delivered")
-    expect(tracking['data']['tracking']["checkpoints"].last["location"]).to  eq("CLIFTON HEIGHTS, PA, 19018")
-    expect(tracking['data']['tracking']["checkpoints"].last["checkpoint_time"]).to  eq("2021-01-09T11:14:00")
+  context "routes" do
+    it { should route(:get, 'companies/1/shipments').to(action: :index, company_id: 1) }
+    it { should route(:get, 'companies/1/shipments/1').to(action: :show, company_id: 1, id: 1) }
+  end
+
+  context "before_action" do
+    it { should use_before_action(:find_company) }
+  end
+  
+  context "tracking info" do
+    it 'can get right tracking info' do
+      shipment = FactoryBot.create(:shipment, 'slug': 'usps', 'tracking_number': '92748902711539543475379057')
+      get :show, params: { company_id: shipment.company.id, id: shipment.id }
+      tracking = AfterShip::V4::Tracking.get(shipment.slug, shipment.tracking_number)
+
+      expect(tracking["data"]["tracking"]["tag"]).to  eq("Delivered")
+      expect(tracking['data']['tracking']["checkpoints"].last["location"]).to  eq("CLIFTON HEIGHTS, PA, 19018")
+      expect(tracking['data']['tracking']["checkpoints"].last["checkpoint_time"]).to  eq("2021-01-09T11:14:00")
+    end
   end
 end
