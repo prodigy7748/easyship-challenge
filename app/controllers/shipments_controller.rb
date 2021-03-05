f075a8444133063c8df77fdd5707fcee1d5fc00f
Phil Lee <prodigy7748g@gmail.com>
2021-03-05 10:31:36 +0800

    add rescue_from for shipment not found

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,10 +1,18 @@
 class ShipmentsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   def index
     @shipments = Shipment.all
   end
 
   def show
-    @company = Company.find_by(id: params[:company_id])
-    @shipment = @company.shipments.find_by(id: params[:id])
+    @company = Company.find_by!(id: params[:company_id])
+    @shipment = @company.shipments.find_by!(id: params[:id])
   end
+
+  def not_found
+    render json: { error: error.message }, status: 404
+  end
+
 end
